b2e74672398474374b6f315da5da93b7e192ff27
Juan Resinas Arias de Reyna <jresinas@emergya.com>
2021-08-04 18:07:02 +0200

    Change team circle sprite and other minor changes

--- Assets/Resources/Others/CharacterTeam/CharacterTeam.cs
+++ Assets/Resources/Others/CharacterTeam/CharacterTeam.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class CharacterTeam : MonoBehaviour {
     [SerializeField] CharacterController character;
     //Color COLOR_PLAYER1 = new Color(255, 0, 0);
-    Color COLOR_PLAYER1 = new Color32(255, 30, 30, 100);
+    //Color COLOR_PLAYER1 = new Color32(255, 30, 30, 100);
+    Color COLOR_PLAYER1 = new Color32(255, 30, 30, 150);
+    //Color COLOR_PLAYER1 = new Color32(120, 15, 15, 150);
     //Color COLOR_PLAYER2 = new Color(0, 125, 255);
-    Color COLOR_PLAYER2 = new Color32(0, 180, 255, 100);
+    //Color COLOR_PLAYER2 = new Color32(0, 180, 255, 100);
+    //Color COLOR_PLAYER2 = new Color32(0, 255, 255, 150);
+    Color COLOR_PLAYER2 = new Color32(0, 100, 255, 150);
+    //Color COLOR_PLAYER2 = new Color32(0, 44, 120, 150);
 
     void Start() {
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
