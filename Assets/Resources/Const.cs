98df302504fe4a742d7e0674b45c3dd838df9fa7
Juan Resinas Arias de Reyna <jresinas@emergya.com>
2021-08-07 19:41:19 +0200

    Minor changes

--- Assets/Resources/Const.cs
+++ Assets/Resources/Const.cs
@@ -11,7 +11,7 @@ public static class Const {
     public static float CHAR_OFFSET = 0.05f;
     public static float WAIT_AFTER_SKILL_ANIM = 1.5f;
     public static float CHAR_FADE_OUT_SECONDS = 2f;
-    public static float PROJ_FADE_OUT_SECONDS = 4f;
+    public static float PROJ_FADE_OUT_SECONDS = 5f;
 
     public static float PORTRAIT_ENDTURN_TIME = 0.25f;
     public static float PORTRAIT_SKIPTURN_TIME = 0.5f;
